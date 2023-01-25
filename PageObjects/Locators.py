7b81e6283017a6576f0bfd2810df364db4bb884f
Ameni <souissiameniisi@gmail.com>
2023-01-24 19:15:30 -0500

    Modifier Locators

--- PageObjects/Locators.py
+++ PageObjects/Locators.py
@@ -1,4 +1,9 @@
 # Locators de la page Login
 txt_LoginuserName = "id:Email"
 txt_LoginPass = "id:Password"
-Btn_LoginBtn = "xpath= //button[@type='submit']"
+Btn_LoginBtn = "xpath= git "
+
+# Locators de la page Acceuil
+
+
+
